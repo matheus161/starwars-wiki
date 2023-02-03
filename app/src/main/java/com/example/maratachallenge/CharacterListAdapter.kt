644cb0b0f2fb95468d34c481ed0e7c09a82505c2
matheus161 <matheeus161@gmail.com>
2023-02-03 14:16:01 -0300

    first commit

---
+++ app/src/main/java/com/example/maratachallenge/CharacterListAdapter.kt
@@ -0,0 +1,11 @@
+package com.example.maratachallenge
+
+import android.view.View
+import androidx.recyclerview.widget.RecyclerView
+
+class CharacterListAdapter : RecyclerView.Adapter<> {
+}
+
+class CharacterViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+
+}
