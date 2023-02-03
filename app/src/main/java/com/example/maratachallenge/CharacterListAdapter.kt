ab531c07e965c610b384d3979893be697023ba82
matheus161 <matheeus161@gmail.com>
2023-02-03 14:50:32 -0300

    implemented recyclerView

--- app/src/main/java/com/example/maratachallenge/CharacterListAdapter.kt
+++ app/src/main/java/com/example/maratachallenge/CharacterListAdapter.kt
@@ -1,11 +1,38 @@
 package com.example.maratachallenge
 
+import android.view.LayoutInflater
 import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 
-class CharacterListAdapter : RecyclerView.Adapter<> {
+class CharacterListAdapter(private val items: ArrayList<String>, private val listener:CharactersClicked): RecyclerView.Adapter<CharacterViewHolder>() {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CharacterViewHolder {
+        val view = LayoutInflater
+            .from(parent.context)
+            .inflate(R.layout.item_character, parent, false)
+        val viewHolder = CharacterViewHolder(view)
+        view.setOnClickListener{
+            listener.onItemClicked(items[viewHolder.adapterPosition])
+        }
+        return viewHolder
+    }
+
+    override fun onBindViewHolder(holder: CharacterViewHolder, position: Int) {
+        var currentItem = items[position]
+        holder.nameView.text = currentItem
+    }
+
+    override fun getItemCount(): Int {
+        return items.size
+    }
+
 }
 
 class CharacterViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+    val nameView: TextView = itemView.findViewById(R.id.name)
+}
 
+interface CharactersClicked {
+    fun onItemClicked(item: String)
 }
--- app/src/main/java/com/example/maratachallenge/MainActivity.kt
+++ app/src/main/java/com/example/maratachallenge/MainActivity.kt
@@ -2,11 +2,12 @@ package com.example.maratachallenge
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), CharactersClicked {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -14,5 +15,21 @@ class MainActivity : AppCompatActivity() {
         val recyclerView = findViewById<RecyclerView>(R.id.rv_characteres)
 
         recyclerView.layoutManager = LinearLayoutManager(this)
+        val items = fetchData()
+        val adapter = CharacterListAdapter(items, this)
+        recyclerView.adapter = adapter
+    }
+
+    private fun fetchData(): ArrayList<String> {
+        val list = ArrayList<String>()
+        for (i in 0 until 100) {
+            list.add("Item $i")
+        }
+
+        return list
+    }
+
+    override fun onItemClicked(item: String) {
+        Toast.makeText(this, "clicked item is $item", Toast.LENGTH_LONG).show()
     }
 }
